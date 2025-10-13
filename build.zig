33ea3ab4ba8030360a7839766efdb1813bfc9d51
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-10-13 16:01:25 -0400

    build.zig: Add kColorPicker and kImageAnnotator

--- build.zig
+++ build.zig
@@ -290,6 +290,9 @@ const qt_modules = &.{
     "Attica/attica",
     // Qt 6 KCodecs
     "KCodecs",
+    // Qt 6 kColorPicker
+    "kColorPicker-Qt6",
+    "kColorPicker-Qt6/kColorPicker",
     // Qt 6 KCompletion
     "KCompletion",
     // Qt 6 KConfig
@@ -305,6 +308,9 @@ const qt_modules = &.{
     // Qt 6 KI18n
     "KI18n",
     "KI18nLocaleData",
+    // Qt 6 kImageAnnotator
+    "kImageAnnotator-Qt6",
+    "kImageAnnotator-Qt6/kImageAnnotator",
     // Qt 6 KItemModels
     "KItemModels",
     // Qt 6 KItemViews
