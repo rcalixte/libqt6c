89e95826cc4f1caa68af03f4344a930473619cc2
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-04-16 16:55:05 -0400

    threading: Update user-facing parameter name

--- src/threading/libqt6cthreading.c
+++ src/threading/libqt6cthreading.c
@@ -2,50 +2,50 @@
 #include "libqt6cthreading.hpp"
 #include "libqt6cthreading.h"
 
-void q_threading_exec_now(void* self, void (*callback)(void*)) {
-    Threading_ExecNow(self, (intptr_t)callback);
+void q_threading_exec_now(void* context, void (*callback)(void*)) {
+    Threading_ExecNow(context, (intptr_t)callback);
 }
 
-void q_threading_exec(void* self, void (*callback)(void*)) {
-    Threading_Exec(self, (intptr_t)callback);
+void q_threading_exec(void* context, void (*callback)(void*)) {
+    Threading_Exec(context, (intptr_t)callback);
 }
 
-void q_threading_exec_unique(void* self, void (*callback)(void*)) {
-    Threading_ExecUnique(self, (intptr_t)callback);
+void q_threading_exec_unique(void* context, void (*callback)(void*)) {
+    Threading_ExecUnique(context, (intptr_t)callback);
 }
 
-void q_threading_exec_once(void* self, void (*callback)(void*)) {
-    Threading_ExecOnce(self, (intptr_t)callback);
+void q_threading_exec_once(void* context, void (*callback)(void*)) {
+    Threading_ExecOnce(context, (intptr_t)callback);
 }
 
-void q_threading_async(void* self, void (*callback)(void*)) {
-    Threading_Async(self, (intptr_t)callback);
+void q_threading_async(void* context, void (*callback)(void*)) {
+    Threading_Async(context, (intptr_t)callback);
 }
 
-void q_threading_async_unique(void* self, void (*callback)(void*)) {
-    Threading_AsyncUnique(self, (intptr_t)callback);
+void q_threading_async_unique(void* context, void (*callback)(void*)) {
+    Threading_AsyncUnique(context, (intptr_t)callback);
 }
 
-void q_threading_async_once(void* self, void (*callback)(void*)) {
-    Threading_AsyncOnce(self, (intptr_t)callback);
+void q_threading_async_once(void* context, void (*callback)(void*)) {
+    Threading_AsyncOnce(context, (intptr_t)callback);
 }
 
-void q_threading_async_unique_once(void* self, void (*callback)(void*)) {
-    Threading_AsyncUniqueOnce(self, (intptr_t)callback);
+void q_threading_async_unique_once(void* context, void (*callback)(void*)) {
+    Threading_AsyncUniqueOnce(context, (intptr_t)callback);
 }
 
-void q_threading_sync(void* self, void (*callback)(void*)) {
-    Threading_Sync(self, (intptr_t)callback);
+void q_threading_sync(void* context, void (*callback)(void*)) {
+    Threading_Sync(context, (intptr_t)callback);
 }
 
-void q_threading_sync_unique(void* self, void (*callback)(void*)) {
-    Threading_SyncUnique(self, (intptr_t)callback);
+void q_threading_sync_unique(void* context, void (*callback)(void*)) {
+    Threading_SyncUnique(context, (intptr_t)callback);
 }
 
-void q_threading_sync_once(void* self, void (*callback)(void*)) {
-    Threading_SyncOnce(self, (intptr_t)callback);
+void q_threading_sync_once(void* context, void (*callback)(void*)) {
+    Threading_SyncOnce(context, (intptr_t)callback);
 }
 
-void q_threading_sync_unique_once(void* self, void (*callback)(void*)) {
-    Threading_SyncUniqueOnce(self, (intptr_t)callback);
+void q_threading_sync_unique_once(void* context, void (*callback)(void*)) {
+    Threading_SyncUniqueOnce(context, (intptr_t)callback);
 }
